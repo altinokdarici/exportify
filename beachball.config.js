bd0d69c54afa52d68c75dd71b0b861a4dc1c27d2
Altinok Darici <altinokd@microsoft.com>
2025-09-16 09:50:37 -0700

    Simplify beachball config to only non-default values

--- beachball.config.js
+++ beachball.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-  bumpDeps: false,
-  tag: 'latest',
-  generateChangelog: true,
-  changedFilesPath: 'change',
-  changeDir: 'change',
-  changelogDir: '.',
-
   // Configure for PR-based releases instead of direct pushes
   publish: false,
   push: false,
-
-  // Allow deleted change files (they get removed during bump)
-  disallowDeletedChangeFiles: false,
-
-  // Git configuration
-  gitTags: true,
-  access: 'public'
+  disallowDeletedChangeFiles: false
 };
